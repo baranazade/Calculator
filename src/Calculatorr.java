485c16de3d631e3606f7f7ccb218f4899876455b
Adem Hergul <medarose90@gmail.com>
2019-01-19 11:57:23 -0500

    write add method

---
+++ src/Calculatorr.java
@@ -0,0 +1,14 @@
+public class Calculatorr {
+    public static void main(String[] args) {
+
+    }
+
+    public static double add(double n1, double n2) {
+
+        double sum = n1 + n2;
+
+        return sum;
+    }
+
+}
+
